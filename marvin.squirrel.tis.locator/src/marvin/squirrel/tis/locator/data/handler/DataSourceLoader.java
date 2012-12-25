80b6c61bd6cf7e9fce1627fb68ae9d1ae56a7a1b
Marvin Wang <mwang@talend.com>
2012-12-25 17:18:30 +0800

    Rename CodeDataLoader.java to DataSourceLoader.java

---
+++ marvin.squirrel.tis.locator/src/marvin/squirrel/tis/locator/data/handler/DataSourceLoader.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package marvin.squirrel.tis.locator.data.handler;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import marvin.squirrel.tis.locator.TISLocatorPlugin;
+import marvin.squirrel.tis.locator.data.model.GlobalModel;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+
+/**
+ * @author Marvin
+ * @date 2012-12-23
+ */
+public class DataSourceLoader {
+
+	private static DataSourceLoader dataLoader;
+	
+	private GlobalModel globalModel;
+	
+	private DataSourceLoader(){
+	}
+	
+	public static DataSourceLoader getInstance(){
+		if(dataLoader == null)
+			dataLoader = new DataSourceLoader();
+		return dataLoader;
+	}
+	
+	public GlobalModel loadData(){
+		globalModel = new GlobalModel();
+		
+		Bundle bundle = Platform.getBundle(TISLocatorPlugin.PLUGIN_ID);
+		URL datasourceURL = FileLocator.find(bundle, new Path("/dataware_house/DataSource.xml"), null);
+		
+		try {
+			URL fileURL = FileLocator.toFileURL(datasourceURL);
+			String path = fileURL.getPath();
+			File datasourceFile = new File(path);
+			String abPath = datasourceFile.getAbsolutePath();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return globalModel;
+	}
+
+	
+	
+	
+	/**
+	 * @return the globalModel
+	 */
+	public GlobalModel getGlobalModel() {
+		return globalModel;
+	}
+
+	/**
+	 * @param globalModel the globalModel to set
+	 */
+	public void setGlobalModel(GlobalModel globalModel) {
+		this.globalModel = globalModel;
+	}
+	
+}
